f7833d7825f2daf3a62d152e971a16f2d8644379
Dale Wyttenbach <dwyttenbach@decare.com>
2023-05-10 11:13:48 -0500

    Added actor parameter

--- go.sh
+++ go.sh
@@ -1,1 +1,5 @@
-open http://localhost:8080/quote
+args=""
+if [ $# -gt 0 ]; then
+    args="?actor=$1"
+fi
+open "http://localhost:8080/quote$args"
--- src/main/java/com/davidparry/chuck/DavidController.java
+++ src/main/java/com/davidparry/chuck/DavidController.java
@@ -1,9 +1,11 @@
 package com.davidparry.chuck;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -13,9 +15,9 @@ public class DavidController {
     WebClient client = WebClient.create("https://api.chucknorris.io/jokes/random?category=dev");
 
     @GetMapping("/quote")
-    public Mono<Quote> getQuote() {
-        return client.get().retrieve().bodyToMono(Quote.class)
-                .map(quote -> new Quote(quote.value().replace("Chuck Norris", "Danny")));
+    public Mono<Quote> quote(String actor) {
+        Mono<Quote> mono = client.get().retrieve().bodyToMono(Quote.class);
+		return StringUtils.hasText(actor) ? mono.map(quote -> new Quote(quote.value().replace("Chuck Norris", actor))) : mono;
     }
 
 }
